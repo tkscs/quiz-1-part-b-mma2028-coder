b4f811360cdba11a908995d88e6b8d5370978391
mma2028-coder <mma2028@kehillahstudent.org>
2025-10-24 11:29:38 -0700

    j

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
 import turtle
+turtle.right(45)
+for i in range(4):
+    turtle.forward(100)
+    turtle.right(90)
 
-# YOUR CODE HERE
+turtle.left(45)
+turtle.forward(75)
+for j in range(4):
+    turtle.right(90)
+    turtle.forward(150)
 
 turtle.exitonclick()
